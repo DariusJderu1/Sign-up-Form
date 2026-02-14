940a83078787a43f01a4cf55ec5ee1f9766b522c
DariusJderu1 <jderudarius@gmail.com>
2026-02-14 17:50:57 +0200

    Add js for password validation

---
+++ index.js
@@ -0,0 +1,28 @@
+const passwordInput = document.getElementById("password");
+const passwordConfirm = document.getElementById("confirm-password");
+const passwordParagraph = document.getElementById("password-paragraph");
+const confirmPasswordParagraph = document.getElementById("confirm-password-paragraph");
+let passwordText = "", passwordConfirmText = "";
+
+
+passwordInput.addEventListener("input", () => {
+
+    passwordText = passwordInput.value;
+    
+    //if the password is not long enough
+    if(parseInt(passwordText.length) < 8)
+        passwordParagraph.innerText = "Password requires minimum 8 charecters";
+    else 
+        passwordParagraph.innerText = "";
+});
+
+passwordConfirm.addEventListener("input", () => {
+
+    passwordConfirmText = passwordConfirm.value;
+
+    if(passwordConfirmText != passwordText)
+        confirmPasswordParagraph.innerText = "Password did not match";
+
+    else 
+        confirmPasswordParagraph.innerText = "";
+});
